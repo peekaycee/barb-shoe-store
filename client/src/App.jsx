6fa342a8edff48a9557487a353b8f728bd74753c
kalu kalu okomba <kalukaluokomba@gmail.com>
2024-08-09 08:57:51 +0100

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
